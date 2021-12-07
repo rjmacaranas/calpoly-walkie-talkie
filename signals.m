1f069405badf431b7869f22cb9bb2513d8afe18e
Richard Macaranas <33107245+rjmacaranas@users.noreply.github.com>
2021-12-07 10:58:29 -0800

    Update signals.m

--- signals.m
+++ signals.m
@@ -1,6 +1,7 @@
 % signals.m
 %   Signals Demonstration
 %   author: RJ Macaranas
+clear; close all; clf;
 
 f = 1; % frequency of sine wave (Hz)
 A = 3; % amplitude of the sine wave
@@ -16,27 +17,28 @@ t = tstart: tstep: tstop;
 x = A * sin(2*pi*f*t); 
 
 % continuous-time (CT) we can use plot
-fig = plot(t, x); title("x(t) = sin(2πft)"); 
+fig = plot(t, x); title("x(t) = sin(2\pi ft)"); 
 xlim([-1 2]); xlabel("t (s)"); ylabel("x(t)");
 pause(5);
-saveas(fig, "figures/1/1.png")
+saveas(fig, "signals_1.png")
 
 % discrete-time (DT) we can use stem
-fig = stem(t, x); title("x[n] = sin(2πfn)"); 
+fig = stem(t, x); title("x[n] = sin(2\pi fn)"); 
 xlim([-1 2]); xlabel("n"); ylabel("x[n]"); 
 pause(5);
-saveas(fig, "figures/1/2.png")
+saveas(fig, "signals_2.png")
 
 
 % to observe time shift, we can redefine x
 tshift = 0.5; % time shift (s)
-plot(t, x); title("x(t) = sin(2πft)"); 
+plot(t, x); title("x(t) = sin(2\pi ft)"); 
 xlim([-1 2]); xlabel("t (s)"); ylabel("x(t)"); 
 pause(2);
 
 for dt = 0: 0.1*tshift: tshift
     x = A * sin(2*pi*f * (t - dt));
-    plot(t, x); title("x(t) = sin(2πf (t - dt)"); 
-    xlim([-1 2]); xlabel("t (s)"); ylabel("x(t)");
+    fig = plot(t, x); title("x(t) = sin(2\pi f (t - dt)"); 
+    xlim([-1 2]); xlabel("t (s)"); ylabel("x(t)"); hold on;
     pause(1);
 end
+saveas(fig, "signals_3.png")
